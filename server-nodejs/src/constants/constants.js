ef54ded116965e9c10e4ebd20d8ab383c5bc27c5
Khoa Tran <38184627+anhkhoa195@users.noreply.github.com>
2024-06-24 00:09:21 +0700

    first commit

---
+++ server-nodejs/src/constants/constants.js
@@ -0,0 +1,6 @@
+const constants = Object.assign({
+  CHAT_ENGINE_PROJECT_ID: "5603581d-4b00-4826-b8db-8ed43ac6ba9c",
+  CHAT_ENGINE_PRIVATE_KEY: "08a20859-6a25-43f5-ab6a-9826e86d86d6",
+});
+
+module.exports = { constants };
